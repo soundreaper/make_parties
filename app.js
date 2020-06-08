631d8194ac7309b639fe1db5679187cf2e6074ee
soundreaper <soundreaper@gmail.com>
2020-06-07 20:28:28 -0700

    Commit before switching to macbook to use postgres

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
   { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
@@ -21,6 +27,16 @@ var events = [
 // INDEX
 app.get('/', (req, res) => {
   res.render('events-index', { events: events });
+})
+
+// CREATE
+app.post('/events', (req, res) => {
+  console.log(req.body);
+})
+
+// NEW
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
 })
 
 // Choose a port to listen on
